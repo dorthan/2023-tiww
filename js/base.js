3e3f8745232a238e70d099850dbd8167c5fa824b
Dorthan <dorthan.pan@gmail.com>
2023-05-27 19:35:55 +0800

    sub

--- js/base.js
+++ js/base.js
@@ -10,6 +10,7 @@ function spanHeight(){
     $('.photo__caption').each(function() {
         var $this = $(this);
         var sectionInnerHeight = $this.find('span').innerHeight()+5;
+        console.log('sectionInnerHeight',sectionInnerHeight)
         $this.css({
             'padding-bottom': sectionInnerHeight
         });
